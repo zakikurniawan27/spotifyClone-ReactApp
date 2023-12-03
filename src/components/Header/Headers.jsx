9c80bc9248e66a219ee693e9c8bd373b832b5640
Muhammad Zaki kurniawan <kurniawan.zaki279@gmail.com>
2023-12-04 01:32:03 +0700

    fetch api current user

--- src/components/Header/Headers.jsx
+++ src/components/Header/Headers.jsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react"
 import ButtonLogin from "../button/ButtonLogin"
+import axios from 'axios'
 
 const Headers = ({token, setToken, tokenLocalStorage}) =>{
 
+    const [me, setMe] = useState('')
+
+    
+
+    useEffect(()=>{
+        (async() =>{
+            if(token){
+                try {
+                    const result = await axios.get(
+                        `https://api.spotify.com/v1/me`,{
+                            headers:{
+                                Authorization: `Bearer ${token}`
+                            }
+                        }
+                    )
+                    setMe(result) 
+                } catch (error) {
+                    if(error.status === 401){
+                        localStorage.removeItem("token")
+                    }
+                    console.log(error)
+                }
+            }
+        })()
+    },[token])
+
+    console.log(me)
     const logout = () =>{
         localStorage.removeItem("token")
         setToken('')
@@ -12,7 +41,7 @@ const Headers = ({token, setToken, tokenLocalStorage}) =>{
             {!token ? 
             <ButtonLogin token={token} setToken={setToken} tokenLocalStorage={tokenLocalStorage} />
             :
-            <p className="text-white" onClick={()=> logout()}>Logout</p> 
+            <p className="text-white" onClick={()=> logout()}>{me?.data?.display_name}</p> 
             }
         </div>
     )
